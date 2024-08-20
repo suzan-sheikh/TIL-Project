d90068ac7f5e4ab8be703d093d01d3a50361bba1
suzan-sheikh <mdsuzanskh@gmail.com>
2024-08-20 09:02:06 +0600

    start my project

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <h1 className="text-3xl font-bold underline">Init start my project </h1>
     </>
   )
 }
