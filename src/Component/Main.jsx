b08e32b2834174863395448885af104d699d72e0
suzan-sheikh <mdsuzanskh@gmail.com>
2024-08-27 22:11:24 +0600

    new commit

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -4,7 +4,7 @@ const Main = () => {
             <h2>This is main</h2>
             <h2>Another heading added</h2>
             <h2>i create next line</h2>
-            <h2>i added new line</h2>
+            <h2>other line</h2>
             
         </div>
     );
